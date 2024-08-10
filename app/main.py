d277fe034ab4136c62e03464b3eb90f2bf192823
Michael Aydinbas <michael.aydinbas@gmail.com>
2024-08-10 19:53:43 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+    conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
