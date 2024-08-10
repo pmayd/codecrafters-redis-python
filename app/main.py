90e149b22765e0cf4c9f91c95c8ebcd206ce79aa
Michael Aydinbas <michael.aydinbas@gmail.com>
2024-08-11 00:11:20 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,26 +1,56 @@
+import selectors
 import socket
-import time
+import types
 
 HOST = "localhost"
 PORT = 6379
 
+sel = selectors.DefaultSelector()
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += b"+PONG\r\n"
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
 
 def main():
-    
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    
-    # listen to incoming data from client
-    # for each "PING" respond with "PONG"
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind((HOST, PORT))
+    lsock.listen()
+    print(f"Listening on {(HOST, PORT)}")
+
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
     while True:
-        time.sleep(0.1)
-        
-        data = conn.recv(1024)
-        
-        if not data:
-            break
-    
-        conn.sendall(b"+PONG\r\n")
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
 
 
 if __name__ == "__main__":
