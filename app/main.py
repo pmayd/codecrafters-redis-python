10731cd57193e7c6260d6715f56742b7ec7c82d8
Michael Aydinbas <michael.aydinbas@gmail.com>
2024-08-14 23:24:55 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,74 +1,21 @@
 import asyncio
-import datetime
 import argparse
 
-from app.redis import Redis, Record
+from app.redis import RedisServer
 
 HOST = "localhost"
 DEFAULT_PORT = 6379
-
-DB = {}
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", "-p", type=int, default=DEFAULT_PORT)
+    parser.add_argument("--replicaof", type=str, default="")
 
     return parser.parse_args()
 
 
-async def handle_client(reader, writer):
-    while True:
-        data = await reader.read(100)
-        print("data received: ", data)
-
-        if not data:
-            break
-
-        command = Redis.parse_command(data)
-        match command:
-            case ["ping"]:
-                writer.write(b"+PONG\r\n")
-            case "echo", arg:
-                writer.write(Redis.str2bulk(arg))
-            case "set", key, value, "px", ttl:
-                if key not in DB:
-                    ttl = int(ttl)
-                    expires_at = datetime.datetime.now() + datetime.timedelta(
-                        milliseconds=ttl
-                    )
-                    DB[key] = Record(value, expires_at)
-                    writer.write(Redis.str2bulk("OK"))
-            case "set", key, value:
-                if key not in DB:
-                    DB[key] = Record(value, None)
-                    writer.write(Redis.str2bulk("OK"))
-            case "get", key:
-                value, ttl = DB.get(key)
-                if ttl is not None and ttl < datetime.datetime.now():
-                    del DB[key]
-                    value = None
-                writer.write(Redis.str2bulk(value))
-            case "info", section:
-                if section == "replication":
-                    writer.write(Redis.str2bulk("role:master"))
-
-    writer.close()
-    await writer.wait_closed()
-
-
-async def main(port: int):
-    server = await asyncio.start_server(
-        handle_client, host=HOST, port=port, reuse_port=True
-    )
-
-    addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-    print(f"Serving on {addrs}")
-
-    async with server:
-        await server.serve_forever()
-
-
 if __name__ == "__main__":
     args = parse_args()
-    asyncio.run(main(args.port))
+    server = RedisServer(host=HOST, port=args.port, replicaof=args.replicaof)
+    asyncio.run(server.run())
--- app/redis.py
+++ app/redis.py
@@ -1,6 +1,8 @@
+import asyncio
 import datetime
 import re
-from typing import Literal, NamedTuple
+from collections import UserDict
+from typing import NamedTuple
 
 
 class Record(NamedTuple):
@@ -8,64 +10,120 @@ class Record(NamedTuple):
     expires_at: datetime.datetime | None
 
 
-class Redis:
+class RedisServer(UserDict):
     # RESP (REdis Serialization Protocol) is the protocol used by Redis to send responses to clients.
+    def __init__(self, host: str, port: int, replicaof: str) -> None:
+        self.host = host
+        self.port = port
+        self.master_host = replicaof.split()[0] if replicaof else ""
+        self.master_port = replicaof.split()[1] if replicaof else 0
+        self.role = "master" if not replicaof else "slave"
+        super().__init__()
 
-    @staticmethod
-    def parse_command(message: bytes) -> list[str]:
-        """Parse the RESP message and return the command.
+    async def run(self) -> None:
+        server = await asyncio.start_server(
+            self.handle_client, host=self.host, port=self.port, reuse_port=True
+        )
 
-        Args:
-            message (bytes): The message received from the client.
+        addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
+        print(f"Serving on {addrs}")
 
-        Returns:
-            list[str]: The command sent by the client.
+        async with server:
+            await server.serve_forever()
 
-        Examples:
-            >>> redis = Redis()
-            >>> redis.parse_command(b"*2\\r\\n$4\\r\\nLLEN\\r\\n$6\\r\\nmylist\\r\\n")
-            ['LLEN', 'mylist']
-        """
-        message = message.decode()
-        assert message[0] == "*", "Invalid message format"
+    async def handle_client(self, reader, writer):
+        while True:
+            data = await reader.read(100)
+            print("data received: ", data)
 
-        parts = re.split("\\r\\n", message)
-        number_of_parts = int(parts[0][1:])
+            if not data:
+                break
 
-        command = []
-        for part in parts[1:]:
-            if not part or part[0] == "$":
-                continue
-            command.append(part)
+            command = parse_command(data)
+            match command:
+                case ["ping"]:
+                    writer.write(b"+PONG\r\n")
+                case "echo", arg:
+                    writer.write(str2bulk(arg))
+                case "set", key, value, "px", ttl:
+                    if key not in self.data:
+                        ttl = int(ttl)
+                        expires_at = datetime.datetime.now() + datetime.timedelta(
+                            milliseconds=ttl
+                        )
+                        self.data[key] = Record(value, expires_at)
+                        writer.write(str2bulk("OK"))
+                case "set", key, value:
+                    if key not in self.data:
+                        self.data[key] = Record(value, None)
+                        writer.write(str2bulk("OK"))
+                case "get", key:
+                    value, ttl = self.data.get(key)
+                    if ttl is not None and ttl < datetime.datetime.now():
+                        del self.data[key]
+                        value = None
+                    writer.write(str2bulk(value))
+                case "info", section:
+                    if section == "replication":
+                        writer.write(str2bulk(f"role:{self.role}"))
 
-        command[0] = command[0].lower()
+        writer.close()
+        await writer.wait_closed()
 
-        assert len(command) == number_of_parts, "Invalid message format"
 
-        return command
+def parse_command(message: bytes) -> list[str]:
+    """Parse the RESP message and return the command.
 
-    @staticmethod
-    def str2bulk(string: str | None) -> bytes:
-        """Create a RESP bulk string.
+    Args:
+        message (bytes): The message received from the client.
 
-        Args:
-            string (str): The string to be converted to a RESP bulk string.
+    Returns:
+        list[str]: The command sent by the client.
 
-        Returns:
-            bytes: The RESP bulk string.
+    Examples:
+        >>> redis = Redis()
+        >>> redis.parse_command(b"*2\\r\\n$4\\r\\nLLEN\\r\\n$6\\r\\nmylist\\r\\n")
+        ['LLEN', 'mylist']
+    """
+    message = message.decode()
+    assert message[0] == "*", "Invalid message format"
 
-        Examples:
-            >>> redis = Redis()
-            >>> redis.create_bulk_string("mylist")
-            b"$6\\r\\nmylist\\r\\n"
-        """
-        if string is None:
-            return b"$-1\r\n"
+    parts = re.split("\\r\\n", message)
+    number_of_parts = int(parts[0][1:])
 
-        return f"${len(string)}\r\n{string}\r\n".encode()
+    command = []
+    for part in parts[1:]:
+        if not part or part[0] == "$":
+            continue
+        command.append(part)
+
+    command[0] = command[0].lower()
+
+    assert len(command) == number_of_parts, "Invalid message format"
+
+    return command
+
+
+def str2bulk(string: str | None) -> bytes:
+    """Create a RESP bulk string.
+
+    Args:
+        string (str): The string to be converted to a RESP bulk string.
+
+    Returns:
+        bytes: The RESP bulk string.
+
+    Examples:
+        >>> redis = Redis()
+        >>> redis.create_bulk_string("mylist")
+        b"$6\\r\\nmylist\\r\\n"
+    """
+    if string is None:
+        return b"$-1\r\n"
+
+    return f"${len(string)}\r\n{string}\r\n".encode()
 
 
 if __name__ == "__main__":
-    redis = Redis()
-    print(redis.parse_command(b"*2\r\n$4\r\nLLEN\r\n$6\r\nmylist\r\n"))
-    print(redis.str2bulk("mylist"))
+    print(parse_command(b"*2\r\n$4\r\nLLEN\r\n$6\r\nmylist\r\n"))
+    print(str2bulk("mylist"))
